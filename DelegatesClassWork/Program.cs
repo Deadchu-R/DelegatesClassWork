16ae61deea5b71ee075c46f58e4f85aae9586e47
Deadchu-R <93131102+Deadchu-R@users.noreply.github.com>
2022-12-14 15:24:48 +0200

    Add project files.

---
+++ DelegatesClassWork/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+LimitedObservableList list = new((s) => !s.StartsWith("s") && !s.StartsWith("S"));
+//list.ListItemAdded += Print;
+list.TryAdd("roee");
+list.TryAdd("smile");
+list.TryAdd("Etai");
+list.TryAdd("Smack");
+list.TryAdd("lol");
+list.PrintAll();
+class LimitedObservableList
+{
+    private List<string> _list = new();
+    private Func<string, bool> _check;
+    public Action ListItemAdded;
+
+    public LimitedObservableList(Func<string , bool> predicate)
+    {
+        _check = predicate;
+    }
+
+    public bool TryAdd(string item) 
+    {
+        if (!_check(item))
+        {
+            return false;
+        }
+
+
+
+        _list.Add(item);
+        ListItemAdded?.Invoke();
+      
+        return true;
+    }
+
+    public void PrintAll()
+    {
+        foreach (var item in _list)
+        {
+            Console.WriteLine(item);
+        }
+            
+    }
+
+}
