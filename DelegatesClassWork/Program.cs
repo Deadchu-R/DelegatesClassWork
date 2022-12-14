a3f57d3b8d8ae40eb7992dc47e0ba73239fef263
Deadchu-R <93131102+Deadchu-R@users.noreply.github.com>
2022-12-14 21:26:10 +0200

    half homework done

--- DelegatesClassWork/Program.cs
+++ DelegatesClassWork/Program.cs
@@ -1,22 +1,62 @@
 ﻿using System;
 
-LimitedObservableList list = new((s) => !s.StartsWith("s") && !s.StartsWith("S"));
+//LimitedObservableList list = new((s) => !s.Contains("s") && !s.Contains("S"));
+LimitedObservableList list = new LimitedObservableList(IsContainLetterS);
+
+
+
 //list.ListItemAdded += Print;
 list.TryAdd("roee");
 list.TryAdd("smile");
+list.TryAdd("stanly");
 list.TryAdd("Etai");
 list.TryAdd("Smack");
 list.TryAdd("lol");
+list.TryAdd("Zero");
+list.TryAdd("IHateKremer");
+list.TryAdd("DorBenDor");
+list.TryAdd("IwantToDie");
+
 list.PrintAll();
+
+//list.PrintAll();
+
+bool IsContainLetterS(string input)
+{
+    if (input.Contains("s") || input.Contains("S"))
+    {
+        return true;
+    }
+    else
+    {
+       return false;
+    }
+}
+
 class LimitedObservableList
 {
+    
     private List<string> _list = new();
     private Func<string, bool> _check;
-    public Action ListItemAdded;
+    public Action<string> ListChanged;
+
+     
+    public void PrintChangedObject(string item)
+    {
+        if (_list.Contains(item))
+        { 
+          Console.WriteLine($"{item} added");
+        }
+        else
+        {
+          Console.WriteLine($"{item} removed");
+        }
+    }
 
     public LimitedObservableList(Func<string , bool> predicate)
     {
         _check = predicate;
+        ListChanged += PrintChangedObject;
     }
 
     public bool TryAdd(string item) 
@@ -25,22 +65,20 @@ class LimitedObservableList
         {
             return false;
         }
-
-
-
         _list.Add(item);
-        ListItemAdded?.Invoke();
+        ListChanged?.Invoke(item);
       
         return true;
     }
 
     public void PrintAll()
     {
+        Console.Write("List Contains [");
         foreach (var item in _list)
         {
-            Console.WriteLine(item);
+            Console.Write($",{item}");
         }
-            
+        Console.Write("]");
     }
 
 }
